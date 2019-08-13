9def0a183d2f1b2ff3ebf166cb93619e30796338
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2019-08-12 23:53:28 -0500

    added StackObjInterface2

---
+++ Stack/src/stackpractice/StackObjInterface2.java
@@ -0,0 +1,9 @@
+package stackpractice;
+
+public interface StackObjInterface2 {
+	public boolean push(String fN, String lN);
+	public String pop();
+	public boolean isFull();
+	public boolean isEmpty();
+
+}
