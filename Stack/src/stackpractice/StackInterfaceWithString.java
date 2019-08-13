e724b652e6f53ce9147514368e106c41d5b182b5
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2019-08-12 23:52:34 -0500

    added StackInterfaceWithString

---
+++ Stack/src/stackpractice/StackInterfaceWithString.java
@@ -0,0 +1,9 @@
+package stackpractice;
+
+public interface StackInterfaceWithString {
+	public boolean push(String item);
+	public String pop();
+	public boolean isFull();
+	public boolean isEmpty();
+
+}
