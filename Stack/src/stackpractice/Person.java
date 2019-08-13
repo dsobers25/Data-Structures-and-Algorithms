b0a350d6ee651fb3392392cce62aab4332842443
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2019-08-12 23:49:22 -0500

    added Person class

---
+++ Stack/src/stackpractice/Person.java
@@ -0,0 +1,14 @@
+package stackpractice;
+
+public class Person {
+	private String name;
+	
+	Person(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return "Name: " + this.name;
+	}
+
+}
