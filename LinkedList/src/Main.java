37798aaad09eededd93df7599db4370d85ef2e6a
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2019-09-10 23:43:58 -0500

    overridden Main class

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
+		// Test 1
 		// this linked list is created with a head node by
 		// the constructor
 //		IntLinkedList list = new IntLinkedList(8);
@@ -16,18 +17,28 @@ public class Main {
 //		list.deleteItem(83);
 //		list.printList();
 		
+		
+		// Test 2
 		// this linked list is empty at its creation and
 		// must have nodes added onto it
-		IntLinkedList_v2 list2 = new IntLinkedList_v2();
-		list2.insertItemLast(9);
-		list2.insertItemLast(7);
-		list2.insertItemLast(54);
-		list2.insertItemLast(2);
+//		IntLinkedList_v2 list2 = new IntLinkedList_v2();
+//		list2.insertItemLast(2);
+//		list2.insertItemLast(7);
+//		list2.insertItemLast(9);
+//		list2.printList();
+//		list2.sortList();
+//		System.out.println("After sorting list...");
+//		list2.printList();
 		
-		list2.printList();
-		System.out.println("************ Removing item 54 from Linked List ************");
-		list2.deleteItem(54);
-		list2.printList();
+		// Test 3
+		Person person1 = new Person("deron", "123");
+		Person person2 = new Person("atori", "973");
+		
+		PersonLinkedList list = new PersonLinkedList(person1);
+		list.insertItemLast(person2);
+		list.printList();
+		
+		
 	}
 
 }
